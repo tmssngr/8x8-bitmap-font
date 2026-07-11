d7084811a9c65a3f951ff60228f6df6a32009007
Thomas Singer <tmssngr@users.noreply.github.com>
2026-07-11 12:42:11 +0200

    rewritten in Rust

---
+++ run.cmd
@@ -0,0 +1,9 @@
+target\debug\bitmap-font-8x8.exe         8x8.png   8x8.bin
+target\debug\bitmap-font-8x8.exe --ascii 8x8.png   8x8.mem
+
+target\debug\bitmap-font-8x8.exe         8x8a.png  8x8a.bin
+target\debug\bitmap-font-8x8.exe --ascii 8x8a.png  8x8a.mem
+
+target\debug\bitmap-font-8x8.exe         8x8s.png  8x8s.bin
+target\debug\bitmap-font-8x8.exe --ascii 8x8s.png  8x8s.mem
+pause
