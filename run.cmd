8a89692293e78979f1b0e75b16c633a9e9a71169
Thomas Singer <tmssngr@users.noreply.github.com>
2026-07-21 21:08:15 +0200

    rewritten in Lisette

---
+++ run.cmd
@@ -0,0 +1,14 @@
+target\bin\font-img-to-bin.exe         8x8.png   8x8.bin
+target\bin\font-img-to-bin.exe --ascii 8x8.png   8x8.mem
+
+target\bin\font-img-to-bin.exe         8x8a.png  8x8a.bin
+target\bin\font-img-to-bin.exe --ascii 8x8a.png  8x8a.mem
+
+target\bin\font-img-to-bin.exe         8x8s.png  8x8s.bin
+target\bin\font-img-to-bin.exe --ascii 8x8s.png  8x8s.mem
+
+target\bin\font-img-to-bin.exe         8x8f.png  8x8f.bin
+target\bin\font-img-to-bin.exe --ascii 8x8f.png  8x8f.mem
+
+target\bin\font-img-to-bin.exe         8x8L.png  8x8L.bin
+target\bin\font-img-to-bin.exe --ascii 8x8L.png  8x8L.mem
